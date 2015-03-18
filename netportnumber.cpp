da53af1c8124b8141070c21b01e331a41d98e1dd
BG <bg4444@list.ru>
2015-03-18 15:15:40 +0300

    Auto commit at Wed Mar 18 15:15:40 MSK 2015

--- netportnumber.cpp
+++ netportnumber.cpp
@@ -3,7 +3,7 @@
 using namespace OPTPARSER;
 using namespace std;
 
-netPortNumber::netPortNumber(const string &portno):no(check_range(stol(portno)))
+netPortNumber::netPortNumber(const string &portno):no(check_range(stoul(portno)))
 {
 
 }
