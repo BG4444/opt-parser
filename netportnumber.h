09330e7ff0f693022a3dcdd9f0d40de1da43b1e1
BG <bg4444@list.ru>
2015-03-18 15:08:34 +0300

    Auto commit at Wed Mar 18 15:08:34 MSK 2015

--- netportnumber.h
+++ netportnumber.h
@@ -14,10 +14,10 @@ namespace OPTPARSER
     private:
         template<typename T> static std::remove_const<decltype(no)>::type check_range(const T& in)
         {
-            if(in < 0)
-            {
-                throw std::logic_error("port number cannot be less than zero");
-            }
+//            if(in < 0)
+//            {
+//                throw std::logic_error("port number cannot be less than zero");
+//            }
 //            if( (std::numeric_limits<T>::max() > std::numeric_limits<decltype(no)>::max()) && in > std::numeric_limits<decltype(no)>::max())
 //            {
 //                throw std::logic_error("port number is greather than" +std::to_string(std::numeric_limits<decltype(no)>::max()));
