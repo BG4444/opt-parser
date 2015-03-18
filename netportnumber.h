913d069df172f5513f06e80c6b55add40042446a
BG <bg4444@list.ru>
2015-03-18 15:05:53 +0300

    Auto commit at Wed Mar 18 15:05:53 MSK 2015

--- netportnumber.h
+++ netportnumber.h
@@ -16,12 +16,12 @@ namespace OPTPARSER
         {
             if(in < 0)
             {
-                throw std::logic_error("port number excesses");
+                throw std::logic_error("port number cannot be less than zero");
             }
-            if(std::numeric_limits<T>::max()>std::numeric_limits<decltype(no)>::max() && in > std::numeric_limits<decltype(no)>::max())
-            {
-                throw std::logic_error("port number is greather than" +std::to_string(std::numeric_limits<decltype(no)>::max()));
-            }
+//            if(std::numeric_limits<T>::max()>std::numeric_limits<decltype(no)>::max() && in > std::numeric_limits<decltype(no)>::max())
+//            {
+//                throw std::logic_error("port number is greather than" +std::to_string(std::numeric_limits<decltype(no)>::max()));
+//            }
             return(in);
         }
     public:
