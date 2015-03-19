6a91af2019cb119b7747f12df73cb5e0f6357568
BG <bg4444@list.ru>
2015-03-19 22:07:44 +0300

    Auto commit at Thu Mar 19 22:07:44 MSK 2015

--- timeinterval.h
+++ timeinterval.h
@@ -8,10 +8,9 @@ namespace OPTPARSER
     class TimeInterval
     {
         static int check_interval(const int& in);
-        const int value;
     public:
         TimeInterval(const std::string& in);
-
+        const int value;
     };
 }
 #endif // TIMEINTERVAL_H
