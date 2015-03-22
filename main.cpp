6855951b1cc6a62997fa0a2f94fbb35a9f2fa784
BG <bg4444@list.ru>
2015-03-22 15:56:03 +0300

    Auto commit at Sun Mar 22 15:56:03 MSK 2015

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <QCoreApplication>
 #include "km5device.h"
 #include "km5deviceoptions.h"
+#include <iostream>
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     KM5DeviceOptions opts(argc,argv);
+    cerr<<hex<<$(opts,"deviceID").id<<' '<<$(opts,"deviceID").id<<endl;
     KM5Device dev(opts);
     return a.exec();
 }
